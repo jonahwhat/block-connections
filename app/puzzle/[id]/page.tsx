7df9ef9a44bd5afdb88129e75757e97bb2bb9043
Jonah <105020079+jonahwhat@users.noreply.github.com>
2025-11-09 10:51:48 -0600

    Fix to top logo styling

--- app/puzzle/[id]/page.tsx
+++ app/puzzle/[id]/page.tsx
@@ -160,12 +160,12 @@ export default function PuzzlePage({ params }: PuzzlePageProps) {
     <>
       <Analytics />
       <div className="flex p-1 flex-col items-center w-full md:w-3/4 lg:w-9/12 xl:w-7/12 2xl:w-6/12 mx-auto mt-1">
-      <div onClick={playSoundYes}>
-        <h1 className={`${alfaSlabOne.className} text-black text-4xl font-bold`} style={{ fontSize: "clamp(1.8rem, 2vw, 2.5rem)" }}>
+      <div className="w-full" onClick={playSoundYes}>
+        <h1 className={`${alfaSlabOne.className} text-center text-black text-4xl font-bold`} style={{ fontSize: "clamp(1.8rem, 2vw, 2.5rem)" }}>
           Craft Connections<span className="text-slate-800 font-normal text-2xl ml-2 font-sans" style={{ fontSize: "clamp(0.7rem, 2vw, 1.1rem)" }}>Puzzle #{ parseInt(id) }</span>
         </h1>
         <hr className="w-full"></hr>
-        <h1 className="text-slate-800 my-1" style={{ fontSize: "clamp(0.85rem, 2vw, 1.0rem)" }}>Group four Minecraft items together that are related!</h1>
+        <h1 className="text-slate-800 my-1 text-center" style={{ fontSize: "clamp(0.85rem, 2vw, 1.0rem)" }}>Group four Minecraft items together that are related!</h1>
         </div>
         <div className="relative w-full">
           <Popup show={popupState.show} message={popupState.message} />
