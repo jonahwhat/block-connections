98b700ac520ec4335e27b9c7f031827e1a00450e
Jonah <105020079+jonahwhat@users.noreply.github.com>
2025-11-09 22:00:00 -0600

    Fix hydration error with Date object

--- app/_components/countdown.tsx
+++ app/_components/countdown.tsx
@@ -1,14 +1,33 @@
+"use client"
+
 import React from 'react';
 import Countdown from "react-countdown";
+import { useState, useEffect } from "react";
+
+
+
+
+
+
 
 export default function CountdownTimer() {
-  const now = new Date()
-  const midnight = now.setHours(24,0,0,0)
+
+  const [hydrated, setHydrated] = useState(false);
+
+  useEffect(() => {setHydrated(true)}, [])
+
+  if(!hydrated) {
+    return null
+  }
+
+  const midnight = new Date()
+  midnight.setHours(24, 0, 0, 0)
 
   return (
       <Countdown 
         date={midnight} 
         daysInHours={true}
+        
       />
   );
 }
