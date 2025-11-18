33299dfb1706615ba377fd6e6f2d77269b76502b
Jonah <105020079+jonahwhat@users.noreply.github.com>
2025-11-18 17:51:24 -0600

    More SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "CraftConnections - Daily Minecraft Puzzle",
-  description: "Group four Minecraft items together that are related. New puzzles added daily!",
-  applicationName: "CraftConnections",
+  description: "Group four Minecraft items together that are related! New puzzles added daily.",
+  applicationName: "CraftConnections - Daily Minecraft Puzzle",
   creator: "CraftConnections",
   authors: {
     name: "CraftConnections",
@@ -49,7 +49,12 @@ export default function RootLayout({
 
     <html lang="en">
       <meta property="og:title" content="CraftConnections - Daily Minecraft Puzzle" />
-      <meta property="og:description" content="Group four Minecraft items together that are related!" />
+      <meta property="og:description" content="Group four Minecraft items together that are related! New puzzles added daily." />
+      <link rel="icon" href="/favicon.ico" sizes="144x144"/>
+      <link rel="icon" href="/favicon-48x48.png" sizes="48x48"/>
+      <link rel="icon" href="/icon-large.png" sizes="192x192"/>
+      <link rel="apple-touch-icon" href="/favicon.png"/>
+      <meta property="og:locale" content="en_US" />
       <body className={inter.className}>
         <div itemScope itemType="https://craftconnections.net">
           <meta itemProp="name" content="CraftConnections - Daily Minecraft Puzzle" />
